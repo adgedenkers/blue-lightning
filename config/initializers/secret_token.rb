6e51ffd3ce28a878b8a1ef8bd324cdd5eddaddc5
Adge Denkers <adge.denkers@gmail.com>
2011-02-01 08:18:59 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlueLightning::Application.config.secret_token = '01f3739632ddd8efc523f498c3cca70a6ce95e4ea5d58e93e106960484793c3d1aaa52fd67ec206d9957cc8e337e2937e4f1b21afb0c048bad287362e19fc528'
