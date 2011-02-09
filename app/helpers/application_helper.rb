57c7cb8174cbb197eb5f3167a223174a2d9666a1
Adge Denkers <adge.denkers@gmail.com>
2011-02-09 08:35:29 -0500

    polished page layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   
+  # Return the logo string
+  def logo
+    logo = image_tag("logo.png", :alt => "Springboard", :class => "round")
+  end
+  
   # Return a title on a per-page basis.
   def title
     base_title = "Blue Lightning"
